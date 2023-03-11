cdba09c395c60719a2b6b6504221ca4f1fb2a7e5
Dave Abrahams <dabrahams@adobe.com>
2023-03-10 23:54:04 -0800

    Add prot to initialization

--- emacs/.emacs-profiles.el
+++ emacs/.emacs-profiles.el
@@ -1,3 +1,5 @@
+; use `--with-profile foo' to launch emacs with the foo profile.
 (("default" . ((user-emacs-directory . "~/.dotemacs")))
  ("new" . ((user-emacs-directory . "~/.emacs.d.new")))
+ ("prot" . ((user-emacs-directory . "~/.emacs.d.prot")))
 )
