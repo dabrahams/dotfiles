16587369937b5ef8affdfd7b73fbe122f1cf0b9f
Dave Abrahams <dabrahams@adobe.com>
2023-03-12 09:24:19 -0700

    Suppress native bytecomp warnings in old emacs config

---
+++ emacs/.emacs.d.old/early-init.el
@@ -0,0 +1,19 @@
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;(setq frame-resize-pixelwise t
+;      frame-inhibit-implied-resize t)
+
+(setq use-dialog-box t
+      use-file-dialog nil
+      inhibit-splash-screen t
+      inhibit-startup-screen t
+      inhibit-startup-echo-area-message user-login-name ; no, I really don't want the GNU message
+      inhibit-startup-buffer-menu t)
+
+;; log any warnings during async native compilation but do not pop up *Warnings* buffer.
+(setq native-comp-async-report-warnings-errors 'silent) ; emacs28 with native compilation
+
+;(add-hook 'window-setup-hook 'toggle-frame-maximized t)
+;(add-hook 'window-setup-hook (lambda () (set-frame-font "Monego-15")))
