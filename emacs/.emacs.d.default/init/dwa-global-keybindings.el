06e0b4533f7801a07018f7a703cb2c23545b999b
Dave Abrahams <dabrahams@adobe.com>
2023-03-12 11:23:21 -0700

    Update dwa-global-keybindings to final structure (but not content)

--- emacs/.emacs.d.default/init/dwa-global-keybindings.el
+++ emacs/.emacs.d.default/init/dwa-global-keybindings.el
@@ -1,25 +1,31 @@
 (require 'dwa-navigation)
 
-;; Undo built-in annoyances
+;; Prepare the keymap for my globally-overriding bindings.
+(unless overriding-terminal-local-map
+  (setq overriding-terminal-local-map (make-sparse-keymap)))
+
+(defmacro dwa/global-key (key-name command &optional predicate)
+  "Bind KEY-NAME to COMMAND in `overriding-terminal-local-map'.
+
+Forwards its arguments to `bind-key', which see."
+  `(bind-key ,key-name ,command overriding-terminal-local-map ,predicate))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Bindings ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(dwa/global-key [(control return)] 'other-window)
+(dwa/global-key [(control shift return)] 'dwa/other-window-backward)
+
+;; Navigation by words
+(dwa/global-key [(control ?,)] 'backward-word)
+(dwa/global-key [(control ?.)] 'forward-word)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Unbindings ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (when window-system
   (global-unset-key [(control ?z)]) ; suspend-frame
   (global-unset-key [f11]) ; toggle-frame-fullscreen
 )
 (global-unset-key [(control ?\\)]) ; toggle-input-method
 (global-unset-key [f10]) ; menu-bar-open
-
-
-;;(bind-key* [(control return)] 'other-window)
-;;(bind-key* [(control shift return)] 'dwa/other-window-backward)
-
-(unless overriding-terminal-local-map
-  (setq overriding-terminal-local-map (make-sparse-keymap)))
-
-(define-key overriding-terminal-local-map [(control return)] 'other-window)
-(define-key overriding-terminal-local-map [(control shift return)] 'dwa/other-window-backward)
-
-;; Navigation by words
-(bind-key* [(control ?,)] 'backward-word)
-(bind-key* [(control ?.)] 'forward-word)
 
 (provide 'dwa-global-keybindings)
