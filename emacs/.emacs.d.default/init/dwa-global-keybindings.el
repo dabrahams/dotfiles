c69a52569af9a1a852790cb147b03799ef90a25b
Dave Abrahams <dabrahams@adobe.com>
2023-03-12 10:30:46 -0700

    Begin filling out global keybindings, truly override Magit's binding

--- emacs/.emacs.d.default/init/dwa-global-keybindings.el
+++ emacs/.emacs.d.default/init/dwa-global-keybindings.el
@@ -1,6 +1,25 @@
 (require 'dwa-navigation)
 
-(bind-key* [(control return)] 'other-window)
-(bind-key* [(control shift return)] 'dwa/other-window-backward)
+;; Undo built-in annoyances
+(when window-system
+  (global-unset-key [(control ?z)]) ; suspend-frame
+  (global-unset-key [f11]) ; toggle-frame-fullscreen
+)
+(global-unset-key [(control ?\\)]) ; toggle-input-method
+(global-unset-key [f10]) ; menu-bar-open
+
+
+;;(bind-key* [(control return)] 'other-window)
+;;(bind-key* [(control shift return)] 'dwa/other-window-backward)
+
+(unless overriding-terminal-local-map
+  (setq overriding-terminal-local-map (make-sparse-keymap)))
+
+(define-key overriding-terminal-local-map [(control return)] 'other-window)
+(define-key overriding-terminal-local-map [(control shift return)] 'dwa/other-window-backward)
+
+;; Navigation by words
+(bind-key* [(control ?,)] 'backward-word)
+(bind-key* [(control ?.)] 'forward-word)
 
 (provide 'dwa-global-keybindings)
